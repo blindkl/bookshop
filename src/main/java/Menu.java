595748cf316b1f4e1573d84443b4310f22658ae7
blindkl <blind_kl@o2.pl>
2019-05-11 15:51:22 +0200

    plus login

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,42 +1,38 @@
 import java.util.Scanner;
 
 public class Menu {
-    static Scanner in = new Scanner( System.in );
+    static Scanner in = new Scanner(System.in);
 
-    static void showListOfBooks()
-    {
-        System.out.println( "Wyświetl listę książek" );
+    static void showListOfBooks() {
+        System.out.println("Wyświetl listę książek");
     }
 
-    static void exit()
-    {
-        System.out.println( "Wyjdź" );
+    static void exit() {
+        System.out.println("Wyjdź");
     }
 
-    static void showMenu()
-    {
-        System.out.print( "Menu:\n 1. Wyświetl listę książek \n 2. Wyjdź " );
+    static void showMenu() {
+        System.out.print("Menu:\n 1. Wyświetl listę książek \n 2. Wyjdź ");
     }
 
-    static void menu()
-    {
+    static void menu() {
         int numberOfOption;
-        do
-        {
+        do {
             showMenu();
             numberOfOption = in.nextInt();
-            switch( numberOfOption )
-            {
-                case 1 : showListOfBooks();
+            switch (numberOfOption) {
+                case 1:
+                    showListOfBooks();
                     break;
-                case 2 : exit();
+                case 2:
+                    exit();
                     break;
             }
         }
-        while( numberOfOption != 2 );
+        while (numberOfOption != 2);
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         menu();
     }
 }
